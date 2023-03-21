ea793c73ee321397f85671375b4e4564489b5e18
Pavel Popov <popovpavelwork@gmail.com>
2023-03-21 19:19:30 +0500

    Lab2

--- src/main/java/com/example/testrestservice/controller/MyController.java
+++ src/main/java/com/example/testrestservice/controller/MyController.java
@@ -3,7 +3,10 @@ package com.example.testrestservice.controller;
 import com.example.testrestservice.model.Request;
 import com.example.testrestservice.model.Response;
 
+import com.example.testrestservice.service.MyModifyService;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -14,9 +17,19 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class MyController {
 
+    private final MyModifyService myModifyService;
+
+    @Autowired
+    public MyController(@Qualifier("ModifyErrorMessage") MyModifyService myModifyService){
+        this.myModifyService = myModifyService;
+    }
+
+
     @PostMapping("/feedback")
     public ResponseEntity<Response> feedback(@RequestBody Request request){
-        log.warn("Запрос пришел");
+
+        log.info("Входящий request : " + String.valueOf(request));
+
         Response response = Response.builder()
                 .uid(request.getUid())
                 .operationUid(request.getOperationUid())
@@ -25,6 +38,9 @@ public class MyController {
                 .errorCode("")
                 .errorMessage("")
                 .build();
+
+        Response responseAfterModify = myModifyService.modify(response);
+        log.info("Исходящий response : " + String.valueOf(responseAfterModify));
 
         return new ResponseEntity<>(response, HttpStatus.OK);
 
---
+++ src/main/java/com/example/testrestservice/service/MyModifyService.java
@@ -0,0 +1,9 @@
+package com.example.testrestservice.service;
+
+import com.example.testrestservice.model.Response;
+
+public interface MyModifyService {
+
+    Response modify(Response response);
+
+}
